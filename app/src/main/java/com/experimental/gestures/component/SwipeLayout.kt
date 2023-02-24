0819245b3b12519398d2c2b9573bfd78b77a15cd
BrandonQ <brandonquintanillaq@gmail.com>
2023-02-24 17:26:31 -0500

    Refactor and cleaning

--- app/src/main/java/com/experimental/gestures/component/SwipeLayout.kt
+++ app/src/main/java/com/experimental/gestures/component/SwipeLayout.kt
@@ -1,17 +1,15 @@
 package com.experimental.gestures.component
 
-import android.animation.*
+import android.animation.AnimatorSet
+import android.animation.ValueAnimator
+import android.annotation.SuppressLint
 import android.content.Context
-import android.graphics.Color
-import android.graphics.drawable.Drawable
 import android.util.AttributeSet
 import android.util.Log
-import android.view.Gravity
 import android.view.MotionEvent
 import android.view.View
-import android.view.ViewGroup
 import android.view.animation.AccelerateDecelerateInterpolator
-import android.widget.*
+import android.widget.FrameLayout
 import androidx.core.view.children
 import com.experimental.gestures.R
 
@@ -27,6 +25,40 @@ class SwipeLayout @JvmOverloads constructor(
     private var initialXEvent = 0f
 
     private var onActiveListener: OnActiveListener? = null
+
+    @SuppressLint("ClickableViewAccessibility")
+    private val buttonTouchListener: OnTouchListener = OnTouchListener { v, event ->
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> childView?.isTouchInside(event) ?: false
+            MotionEvent.ACTION_MOVE -> {
+
+                Log.i("TAG", "onTouch event.x: " + event.x)
+                Log.i("TAG", "onTouch childView!!.width: " + childView!!.width)
+
+                if (initialXEvent == 0f) {
+                    initialXEvent = event.x
+                }
+
+                //attach to center
+                //swipeButtonInner!!.x = event.x - swipeButtonInner!!.width / 2
+
+                //attach to touch point
+                childView!!.x = event.x - initialXEvent
+
+                true
+            }
+            MotionEvent.ACTION_UP -> {
+
+                if (childView!!.x + childView!!.width > width * 0.9) {
+                    onActiveListener?.onActive()
+                }
+                returnToOriginalPosition()
+
+                true
+            }
+            else -> false
+        }
+    }
 
     init {
         setup(ctx, attrs)
@@ -45,51 +77,15 @@ class SwipeLayout @JvmOverloads constructor(
         setOnTouchListener(buttonTouchListener)
     }
 
-    private val buttonTouchListener: OnTouchListener
-        get() = object : OnTouchListener {
-            override fun onTouch(v: View, event: MotionEvent): Boolean {
-                when (event.action) {
-                    MotionEvent.ACTION_DOWN -> return !isTouchOutsideInitialPosition(
-                        event, childView!!
-                    )
-                    MotionEvent.ACTION_MOVE -> {
+    private fun returnToOriginalPosition() {
+        Log.i("TAG", "onTouch childView!!.width: " + childView!!.width)
 
-                        Log.i("TAG", "onTouch event.x: " + event.x)
-
-                        if (initialXEvent == 0f) {
-                            initialXEvent = event.x
-                        }
-
-                        //attach to center
-                        //swipeButtonInner!!.x = event.x - swipeButtonInner!!.width / 2
-
-                        //attach to touch point
-                        childView!!.x = event.x - initialXEvent
-
-                        return true
-                    }
-                    MotionEvent.ACTION_UP -> {
-
-                        if (childView!!.x + childView!!.width > width * 0.9) {
-                            onActiveListener?.onActive()
-                            moveButtonBack()
-                        } else {
-                            moveButtonBack()
-                        }
-
-                        return true
-                    }
-                }
-                return false
-            }
-        }
-
-    private fun moveButtonBack() {
         val positionAnimator = ValueAnimator.ofFloat(childView!!.x, 0f)
         positionAnimator.interpolator = AccelerateDecelerateInterpolator()
         positionAnimator.addUpdateListener {
             val x = positionAnimator.animatedValue as Float
             childView!!.x = x
+            Log.i("TAG", "onTouch childView!!.width: " + childView!!.width)
         }
         positionAnimator.duration = 200
         val animatorSet = AnimatorSet()
@@ -97,10 +93,10 @@ class SwipeLayout @JvmOverloads constructor(
         animatorSet.start()
         initialXEvent = 0f
     }
+}
 
-
-    companion object {
-        private const val ENABLED = 0
-        private const val DISABLED = 1
-    }
+fun View.isTouchInside(event: MotionEvent): Boolean {
+    val isBeforeLeft = event.x <= this.x + this.width
+    val isAfterRight = event.x > this.x
+    return isBeforeLeft && isAfterRight
 }
