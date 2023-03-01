fc028d9195d610e1e55efdb7cbfd0d97fa773d3a
BrandonQ <brandonquintanillaq@gmail.com>
2023-03-01 10:07:43 -0500

    Code cleaning

--- app/src/main/java/com/experimental/gestures/githubdemo/CustomSwipeAdapter.kt
+++ app/src/main/java/com/experimental/gestures/githubdemo/CustomSwipeAdapter.kt
@@ -18,39 +18,26 @@ import com.experimental.gestures.github.library.SwipeLayout
  * Created by Brandon Quintanilla on Feb/28/2023
  */
 
-
-/*class CustomSwipeAdapter {
-}*/
 class CustomSwipeAdapter(context: Context, objects: ArrayList<String>) :
     RecyclerSwipeAdapter<CustomSwipeAdapter.SimpleViewHolder?>() {
     class SimpleViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         var swipeLayout: SwipeLayout
-        //var textViewPos: TextView
-        //var textViewData: TextView
-        //var buttonDelete: Button
 
         init {
             swipeLayout = itemView.findViewById<View>(R.id.swipe) as SwipeLayout
-            //textViewPos = itemView.findViewById<View>(R.id.position) as TextView
-            //textViewData = itemView.findViewById<View>(R.id.text_data) as TextView
-            //buttonDelete = itemView.findViewById<View>(R.id.delete) as Button
-            itemView.setOnClickListener(object : View.OnClickListener {
-                override fun onClick(view: View) {
-                    //Log.d(javaClass.simpleName, "onItemSelected: " + textViewData.text.toString())
-                    Toast.makeText(
-                        view.context,
-                        "onItemSelected: $adapterPosition",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
-            })
+            itemView.setOnClickListener { view ->
+                Toast.makeText(
+                    view.context,
+                    "onItemSelected: $adapterPosition",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
         }
     }
 
     private val mContext: Context
     private val mDataset: ArrayList<String>
 
-    //protected SwipeItemRecyclerMangerImpl mItemManger = new SwipeItemRecyclerMangerImpl(this);
     init {
         mContext = context
         mDataset = objects
@@ -64,8 +51,7 @@ class CustomSwipeAdapter(context: Context, objects: ArrayList<String>) :
 
     override fun onBindViewHolder(viewHolder: SimpleViewHolder, position: Int) {
         val item = mDataset[position]
-        //viewHolder.swipeLayout.showMode = SwipeLayout.ShowMode.LayDown
-        viewHolder.swipeLayout.showMode = SwipeLayout.ShowMode.PullOut
+        viewHolder.swipeLayout.showMode = SwipeLayout.ShowMode.LayDown
         val starBottView: View = viewHolder.swipeLayout.findViewById(R.id.starbott)
         viewHolder.swipeLayout.addDrag(SwipeLayout.DragEdge.Left, viewHolder.swipeLayout.findViewById(R.id.bottom_wrapper))
         viewHolder.swipeLayout.addDrag(SwipeLayout.DragEdge.Right, viewHolder.swipeLayout.findViewById(R.id.bottom_wrapper_2))
@@ -73,8 +59,7 @@ class CustomSwipeAdapter(context: Context, objects: ArrayList<String>) :
         viewHolder.swipeLayout.addDrag(SwipeLayout.DragEdge.Bottom, starBottView)
         viewHolder.swipeLayout.addSwipeListener(object : SimpleSwipeListener() {
             override fun onOpen(layout: SwipeLayout) {
-                //YoYo.with(Techniques.Tada).duration(500).delay(100).playOn(layout.findViewById(R.id.trash));
-                Toast.makeText(mContext, "ON_SIDE_CLICK", Toast.LENGTH_SHORT).show()
+                Toast.makeText(mContext, "SWIPED", Toast.LENGTH_SHORT).show()
             }
         })
         viewHolder.swipeLayout.setOnDoubleClickListener { layout, surface ->
@@ -84,20 +69,7 @@ class CustomSwipeAdapter(context: Context, objects: ArrayList<String>) :
                 Toast.LENGTH_SHORT
             ).show()
         }
-        /*viewHolder.buttonDelete.setOnClickListener { view ->
-            mItemManger.removeShownLayouts(viewHolder.swipeLayout)
-            mDataset.removeAt(position)
-            notifyItemRemoved(position)
-            notifyItemRangeChanged(position, mDataset.size)
-            mItemManger.closeAllItems()
-            Toast.makeText(
-                view.context,
-                "Deleted ", //+ viewHolder.textViewData.text.toString() + "!",
-                Toast.LENGTH_SHORT
-            ).show()
-        }*/
-        //viewHolder.textViewPos.text = (position + 1).toString() + "."
-        //viewHolder.textViewData.text = item
+
         mItemManger.bind(viewHolder.itemView, position)
     }
 
